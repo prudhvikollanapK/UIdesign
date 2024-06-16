33413d363da01dd01dedd21d88ae6d17c9604df2
pK <86195686+prudhvikollanapK@users.noreply.github.com>
2024-06-16 15:21:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ButtonSection from './ButtonSection';
+import CheckboxSection from './CheckboxSection';
+import DropdownSection from './DropdownSection';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonSection />
+      <CheckboxSection />
+      <DropdownSection />
+    </div>
+  );
+}
+
+export default App;
